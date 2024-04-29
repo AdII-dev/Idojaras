bd6d5ef461d0e3dd1b7c2cee774148ef841b7ef1
AdII-dev <78079623+AdII-dev@users.noreply.github.com>
2024-04-29 11:17:45 +0200

    Update MainWindow.xaml.cs

--- Idojaras/MainWindow.xaml.cs
+++ Idojaras/MainWindow.xaml.cs
@@ -1,7 +1,6 @@
-﻿using System.Text;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -17,41 +16,101 @@ namespace Idojaras
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Varos> varosok = new();
+        public List<string> adat { get; set; }
         public MainWindow()
         {
             InitializeComponent();
-        
-        
-        
-        
-        
-        }
-        private void VarosAdd_GotFocus(object sender, RoutedEventArgs e)
+            P.Visibility = Visibility.Hidden;
+            H.Visibility = Visibility.Hidden;
+            SZ.Visibility = Visibility.Hidden;
+            PL.Visibility = Visibility.Hidden;
+            HL.Visibility = Visibility.Hidden;
+            SZL.Visibility = Visibility.Hidden;
+            telepules.Visibility = Visibility.Hidden;
+            Done.Visibility = Visibility.Hidden;
+            varosok.Add(new("Miskolc", 15, 45, 39));
+            varosok.Add(new("Budapest", 20, 42, 32));
+            varosok.Add(new("Székesfehérvár", 12, 36, 37));
+            Feltolt(varosok);
+        }   
+
+        private void Varosok_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (VarosAdd.Text == "Város Neve")
+            if (Varosok.SelectedItem != null)
             {
-                VarosAdd.Text = string.Empty;
+                ListBoxItem varos = (ListBoxItem)Varosok.SelectedItem;
+                bool kellAdat = true;
+                foreach (var v in varosok)
+                {
+                    if (v._nev == varos.Content.ToString())
+                    {
+                        Adatok.Text = $"Páratartalom: {v._homerseklet.ToString()} g/m3 \n Hőméréklet: {v._paratartalom.ToString()} °C\n Szélsebesség: {v._szelsebesseg.ToString()} km/h";
+                        kellAdat = false;
+                    }
+                }
+                if (kellAdat)
+                {
+                    P.Visibility = Visibility.Visible;
+                    H.Visibility = Visibility.Visible;
+                    SZ.Visibility = Visibility.Visible;
+                    PL.Visibility = Visibility.Visible;
+                    HL.Visibility = Visibility.Visible;
+                    SZL.Visibility = Visibility.Visible;
+                    telepules.Visibility = Visibility.Visible;
+                    Done.Visibility = Visibility.Visible;
+                    telepules.Text = varos.Content.ToString();
+                }
             }
         }
 
-        private void VarosAdd_TextChanged(object sender, TextChangedEventArgs e)
+        private void Hozzaad(object sender, RoutedEventArgs e)
         {
-            
+            ListBoxItem item = new();
+            item.Content = Telepules.Text;
+            Varosok.Items.Add(item);
+            Telepules.Text = string.Empty;
         }
 
-        private void homersekletAdd_TextChanged(object sender, TextChangedEventArgs e)
+        private void Feltolt(List<Varos> varosok)
         {
-
+            Varosok.Items.Clear();
+            foreach (var v in varosok)
+            {
+                ListBoxItem varos = new();
+                varos.Content = v._nev;
+                Varosok.Items.Add(varos);
+            }
         }
 
-        private void paraAdd_TextChanged(object sender, TextChangedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            varosok.Add(new(telepules.Text, int.Parse(H.Text), int.Parse(P.Text), int.Parse(SZ.Text)));
+            P.Visibility = Visibility.Hidden;
+            H.Visibility = Visibility.Hidden;
+            SZ.Visibility = Visibility.Hidden;
+            PL.Visibility = Visibility.Hidden;
+            HL.Visibility = Visibility.Hidden;
+            SZL.Visibility = Visibility.Hidden;
+            telepules.Visibility = Visibility.Hidden;
+            Done.Visibility = Visibility.Hidden;
+            Feltolt(varosok);
         }
 
-        private void szelsebessegAdd_TextChanged(object sender, TextChangedEventArgs e)
+        private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-
+            if (Adatok.Text != string.Empty) Adatok.Text = string.Empty;
+            ListBoxItem item = (ListBoxItem)Varosok.SelectedItem;
+            int i = -1;
+            foreach (var v in varosok)
+            {
+                if (v._nev == item.Content.ToString())
+                {
+                    i = varosok.IndexOf(v);
+                }
+            }
+            if(i  > -1 )varosok.RemoveAt(i);
+            Feltolt(varosok);
         }
     }
 }
