923658e55b37447f7b3e27e52d8d11da32060042
AdII-dev <78079623+AdII-dev@users.noreply.github.com>
2024-04-29 11:19:28 +0200

    Update MainWindow.xaml.cs

--- Idojaras/MainWindow.xaml.cs
+++ Idojaras/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
+using System.Security.Policy;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -21,59 +23,19 @@ namespace Idojaras
         public MainWindow()
         {
             InitializeComponent();
-            P.Visibility = Visibility.Hidden;
-            H.Visibility = Visibility.Hidden;
-            SZ.Visibility = Visibility.Hidden;
-            PL.Visibility = Visibility.Hidden;
-            HL.Visibility = Visibility.Hidden;
-            SZL.Visibility = Visibility.Hidden;
-            telepules.Visibility = Visibility.Hidden;
-            Done.Visibility = Visibility.Hidden;
+
+            //Alapértelmezett adatok felvitele
             varosok.Add(new("Miskolc", 15, 45, 39));
             varosok.Add(new("Budapest", 20, 42, 32));
             varosok.Add(new("Székesfehérvár", 12, 36, 37));
+            //Új adatok felvitele
             Feltolt(varosok);
-        }   
 
-        private void Varosok_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (Varosok.SelectedItem != null)
-            {
-                ListBoxItem varos = (ListBoxItem)Varosok.SelectedItem;
-                bool kellAdat = true;
-                foreach (var v in varosok)
-                {
-                    if (v._nev == varos.Content.ToString())
-                    {
-                        Adatok.Text = $"Páratartalom: {v._homerseklet.ToString()} g/m3 \n Hőméréklet: {v._paratartalom.ToString()} °C\n Szélsebesség: {v._szelsebesseg.ToString()} km/h";
-                        kellAdat = false;
-                    }
-                }
-                if (kellAdat)
-                {
-                    P.Visibility = Visibility.Visible;
-                    H.Visibility = Visibility.Visible;
-                    SZ.Visibility = Visibility.Visible;
-                    PL.Visibility = Visibility.Visible;
-                    HL.Visibility = Visibility.Visible;
-                    SZL.Visibility = Visibility.Visible;
-                    telepules.Visibility = Visibility.Visible;
-                    Done.Visibility = Visibility.Visible;
-                    telepules.Text = varos.Content.ToString();
-                }
-            }
-        }
-
-        private void Hozzaad(object sender, RoutedEventArgs e)
-        {
-            ListBoxItem item = new();
-            item.Content = Telepules.Text;
-            Varosok.Items.Add(item);
-            Telepules.Text = string.Empty;
         }
 
         private void Feltolt(List<Varos> varosok)
         {
+            //Város adatok feltöltése
             Varosok.Items.Clear();
             foreach (var v in varosok)
             {
@@ -83,34 +45,83 @@ namespace Idojaras
             }
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+
+        private void VarosAdd_GotFocus(object sender, RoutedEventArgs e)
         {
-            varosok.Add(new(telepules.Text, int.Parse(H.Text), int.Parse(P.Text), int.Parse(SZ.Text)));
-            P.Visibility = Visibility.Hidden;
-            H.Visibility = Visibility.Hidden;
-            SZ.Visibility = Visibility.Hidden;
-            PL.Visibility = Visibility.Hidden;
-            HL.Visibility = Visibility.Hidden;
-            SZL.Visibility = Visibility.Hidden;
-            telepules.Visibility = Visibility.Hidden;
-            Done.Visibility = Visibility.Hidden;
-            Feltolt(varosok);
+            //Fókuszban kitörli az alapértelmezett szöveget
+            if (VarosAdd.Text == "Város Neve")
+            {
+                VarosAdd.Text = string.Empty;
+            }
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+
+        private void homersekletAdd_GotFocus(object sender, RoutedEventArgs e)
         {
-            if (Adatok.Text != string.Empty) Adatok.Text = string.Empty;
-            ListBoxItem item = (ListBoxItem)Varosok.SelectedItem;
-            int i = -1;
-            foreach (var v in varosok)
+            //Fókuszban kitörli az alapértelmezett szöveget
+            if (homersekletAdd.Text == "Város Hőmérséklete")
             {
-                if (v._nev == item.Content.ToString())
-                {
-                    i = varosok.IndexOf(v);
-                }
+                homersekletAdd.Text = string.Empty;
             }
-            if(i  > -1 )varosok.RemoveAt(i);
-            Feltolt(varosok);
+
+        }
+
+        private void paraAdd_GotFocus(object sender, RoutedEventArgs e)
+        {
+            //Fókuszban kitörli az alapértelmezett szöveget
+            if (paraAdd.Text == "Város Páratartalma")
+            {
+                paraAdd.Text = string.Empty;
+            }
+        }
+
+        private void szelsebessegAdd_GotFocus(object sender, RoutedEventArgs e)
+        {
+            //Fókuszban kitörli az alapértelmezett szöveget
+            if (szelsebessegAdd.Text == "Város Szélsebessége")
+            {
+                szelsebessegAdd.Text = string.Empty;
+            }
+        }
+
+
+
+        List<string> homerseklet = [];
+        List<string> para = [];
+        List<string> szel = [];
+        private void AdatokBevitele_Click(object sender, RoutedEventArgs e)
+        {
+            //Eltárolja a beírt adatokat
+            Varosok.Items.Add(VarosAdd.Text);
+            homerseklet.Add(homersekletAdd.Text);
+            para.Add(paraAdd.Text);
+            szel.Add(szelsebessegAdd.Text);
+
+            //Visszaállítja az alapértelmezett szöveget
+            VarosAdd.Text = "Város Neve";
+            homersekletAdd.Text = "Város Hőmérséklete";
+            paraAdd.Text = "Város Páratartalma";
+            szelsebessegAdd.Text = "Város Szélsebessége";
+
+        }
+
+
+        private void Varosok_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (Varosok.SelectedItem != null)
+            {
+                ListBoxItem varos = (ListBoxItem)Varosok.SelectedItem;
+                
+                foreach (var v in varosok)
+                {
+                    if (v._nev == varos.Content.ToString())
+                    {
+                        Adatok.Text = $"Páratartalom: {v._homerseklet.ToString()} g/m3 \n Hőméréklet: {v._paratartalom.ToString()} °C\n Szélsebesség: {v._szelsebesseg.ToString()} km/h";
+                        
+                    }
+                }
+               
+            }
         }
     }
 }
